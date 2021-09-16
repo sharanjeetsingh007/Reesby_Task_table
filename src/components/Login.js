796a35a374035e6c4fc33031db403a35ae262377
sharan <shazan.singh@gmail.com>
2021-09-16 10:30:01 +1000

    changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,37 +22,51 @@ export default function Login() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [validate, setValidate] = useState("")
 
     const dispatch = useDispatch();
-
 
     // using login action reducer
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        dispatch(login({
-            name: name,
-            email: email,
-            password: password,
-            loggedIn: true,
-        }));
+        if (!name) {
+            let errorname = 'Please enter name';
+            return setValidate(errorname);
 
+        } else if (!email) {
+
+            let erroremail = 'Please enter email';
+            return setValidate(erroremail);
+
+        } else if (!password) {
+
+            let errorpass = 'Please enter password';
+            return setValidate(errorpass);
+
+        } else {
+
+            dispatch(login({
+                name: name,
+                email: email,
+                password: password,
+                loggedIn: true,
+            }));
+        }
     }
 
-
     return (
-
 
         <div className='main'>
             <div className='login'>
                 <h2>Login</h2>
-
                 <form className={classes.root} noValidate autoComplete="off"
                     onSubmit={e => handleSubmit(e)}>
                     <div className='input'>
                         <TextField
                             id="standard-basic"
                             label="Username"
+
                             value={name}
                             onChange={e => { setName(e.target.value) }}
                             style={{ width: 600, marginTop: 0 }} />
@@ -73,17 +87,18 @@ export default function Login() {
                             onChange={e => { setPassword(e.target.value) }}
                             style={{ width: 600 }} />
                     </div>
-
+                    <div
+                        style={{ width: '300px', height: '20px', color: 'red', padding: '1px', marginTop: '-4px' }}>
+                        {validate}
+                    </div>
                     <div className='login-button'>
                         <Button
                             type='submit'
-                            style={{ width: 300, height: 39, backgroundColor: '#5900B4', color: '#FFFFFF', marginTop: '10px' }}
+                            style={{ width: 300, height: 39, backgroundColor: '#5900B4', color: '#FFFFFF', marginTop: '-13px' }}
                             diaplay='flex'>
                             Login In
                         </Button>
                     </div>
-
-
                 </form>
             </div>
         </div>
